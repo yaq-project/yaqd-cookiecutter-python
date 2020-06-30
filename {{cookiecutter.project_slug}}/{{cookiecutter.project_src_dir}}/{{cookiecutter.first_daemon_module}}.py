fa9303d8d2bf574b71c091e240dbfe30b6571202
Kyle Sunden <git@ksunden.space>
2020-06-30 01:19:40 -0500

    Move to flit, avro, simplify generated code

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_src_dir}}/{{cookiecutter.first_daemon_module}}.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_src_dir}}/{{cookiecutter.first_daemon_module}}.py
@@ -5,51 +5,17 @@ from typing import Dict, Any, List
 
 from yaqd_core import {{ cookiecutter.base_class }}
 
-from .__version__ import __branch__
-
 
 class {{ cookiecutter.class_name }}({{ cookiecutter.base_class }}):
     _kind = "{{ cookiecutter.first_daemon_kind }}"
-    _version = "0.1.0" + f"+{__branch__}" if __branch__ else ""
-    traits: List[str] = []
-    defaults: Dict[str, Any] = {}
 
     def __init__(self, name, config, config_filepath):
         super().__init__(name, config, config_filepath)
         # Perform any unique initialization
 {% if cookiecutter.base_class == "Sensor" %}
-        self.channel_names = ["channel"]
-        self.channel_units = {"channel": "units"}
-{% endif %}
+        self._channel_names = ["channel"]
+        self._channel_units = {"channel": "units"}
 
-    def _load_state(self, state):
-        """Load an initial state from a dictionary (typically read from the state.toml file).
-
-        Must be tolerant of missing fields, including entirely empty initial states.
-
-        Parameters
-        ----------
-        state: dict
-            The saved state to load.
-        """
-        super()._load_state(state)
-        # This is an example to show the symetry between load and get
-        # If no persistent state is needed, these unctions can be deleted
-        self.value = state.get("value", 0)
-
-    def get_state(self):
-        state = super().get_state()
-        state["value"] = self.value
-        return state
-
-{% if cookiecutter.base_class == "DiscreteHardware" %}
-    def get_value(self):
-        return self.value
-
-    def set_value(self, value):
-        ...
-{% endif %}
-{% if cookiecutter.base_class == "Sensor" %}
     async def _measure(self):
         return {"channel": 0}
 {% endif %}
